b8a7f16c6d84b40e5a7a6654d5e6c5dd1bea43e1
jimas95 <jimas95@hotmail.com>
2021-09-29 22:55:11 -0500

    added second obj for display, service for getting grasping pregrasping positions

--- arm/src/arm/candle.py
+++ arm/src/arm/candle.py
@@ -9,11 +9,12 @@ from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
 from geometry_msgs.msg import Vector3,Pose,Point, Quaternion
 from std_msgs.msg import ColorRGBA
+from arm.srv import get_eef_goal,get_eef_goalResponse,get_eef_goalRequest
 
 class CANDLE():
 
 
-    def __init__(self):
+    def __init__(self,status):
 
         self.candle_size = Vector3(x=0.035,y=0.035,z=0.01)  # x,y diameter z  height
         self.arrow_size  = Vector3(x=0.025,y=0.01,z=0.01)    # x  length   y,z size
@@ -22,14 +23,15 @@ class CANDLE():
         self.colorRed      = ColorRGBA(r=1.0,g=0.0,b=0.0,a=1.0) # Red
         self.colorYellow   = ColorRGBA(r=1.0,g=1.0,b=0.0,a=1.0) # Yellow
         self.colorGreen    = ColorRGBA(r=0.0,g=1.0,b=0.0,a=1.0) # Green
+        self.colorPurple   = ColorRGBA(r=1.0,g=0.0,b=1.0,a=1.0) # Green
+        
+
+        self.polar = {"r":0.2,"th":0}
+        self.offset_grasp =     [-0.05, 0.10]
+        self.offset_pregrasp =  [-0.10, 0.15]
 
 
-        self.polar = {"r":1,"th":0}
-        self.offset_grasp = [-0.05,0.05]
-        self.offset_pregrasp = [-0.1,0.1]
-
-
-        topic = 'candle_marker_array'
+        topic = 'candle_marker_'+str(status)
         self.publisher = rospy.Publisher(topic, MarkerArray,queue_size=10)
 
         self.markerArray = MarkerArray()
@@ -38,6 +40,10 @@ class CANDLE():
         self.markerArray.markers.append(Marker())
         self.markerType = {"CYLINDER": 3, "ARROW":0}
         
+        rospy.Service('/candle/get_eef_goal_'+str(status), get_eef_goal, self.srvf_get_pose)
+
+        self.colorCandle = self.colorYellow
+        if (status=="place"): self.colorCandle = self.colorPurple
 
     def new_candle(self):
         rospy.logdebug("CANDLE--> creating new Candle position")
@@ -46,8 +52,8 @@ class CANDLE():
         
 
     def random_pose(self):
-        # self.polar['r'] = random.random()/2.0
-        self.polar['theta'] = 2*pi*random.random()
+        # self.polar['r'] = 0.1+random.random()*0.1
+        self.polar['theta'] = pi*(random.random()-0.5)
         
 
     def get_candle_point(self,offset):
@@ -72,7 +78,7 @@ class CANDLE():
         # add candle marker
         self.add_marker(    type=self.markerType['CYLINDER'],
                             size=self.candle_size,
-                            color=self.colorYellow,
+                            color=self.colorCandle,
                             pose=self.get_candle_pose(),
                             id=0)
         # add grasp pose arrow
@@ -111,3 +117,13 @@ class CANDLE():
         pose = Pose(position = pos, orientation=ori)
         return pose
 
+
+    """Service get eef goal
+    Empty request
+    returns the geometry pose of grasp and pregrasp pos
+    """
+    def srvf_get_pose(self,get_eef_goalRequest):
+        msg = get_eef_goalResponse()
+        msg.grasp = self.get_grasp_pose()
+        msg.pregrasp = self.get_pregrasp_pose()
+        return msg
