0fb7337f098501e9ae53f5ff3d20451e2eed853c
jimas95 <jimas95@hotmail.com>
2021-11-09 18:07:16 -0600

    cleaned code

--- builder/nodes/navigate.py
+++ builder/nodes/navigate.py
@@ -21,6 +21,8 @@ NAVIGATE your Turtlebot3!
 
 MAX_ROTATION_SPEED = 1.5
 MAX_LINEAR_SPEED = 0.5
+MIN_DIST_THRESHOLD = 0.05
+MIN_DIR_THRESHOLD = 10
 TO_DEGREE = 57.2958
 
 class GotoPoint():
@@ -32,12 +34,12 @@ class GotoPoint():
         self.tf_listener = tf.TransformListener()
         self.odom_frame = 'world'
         self.base_frame = 'base_footprint'
+        self.hunt_frame = 'hunt_point'
 
         self.hunt_pt = Vector3()
 
-        rospy.Service('/Navigate/ReverseMode', SetBool, self.setReverseMode)
+        rospy.Service('/navigate/reverse_mode', SetBool, self.setReverseMode)
 
-        self.reverse = False
         self.reverse = True
 
         self.tfBuffer = tf2_ros.Buffer()
@@ -47,10 +49,10 @@ class GotoPoint():
         # wait for transforms to be published
         try:
             self.tf_listener.waitForTransform(self.odom_frame, self.base_frame  , rospy.Time(), rospy.Duration(5.0))
-            self.tf_listener.waitForTransform(self.base_frame, 'hunt_point'     , rospy.Time(), rospy.Duration(5.0))
-            self.tf_listener.waitForTransform('base_footreverse', 'hunt_point'  , rospy.Time(), rospy.Duration(5.0))
+            self.tf_listener.waitForTransform(self.base_frame, self.hunt_frame     , rospy.Time(), rospy.Duration(5.0))
+            self.tf_listener.waitForTransform('base_footreverse', self.hunt_frame  , rospy.Time(), rospy.Duration(5.0))
         except (tf.Exception, tf.ConnectivityException, tf.LookupException):
-            rospy.loginfo("Cannot find transform between odom and base_footprint or for hunt point")
+            rospy.logerr("NAVIGATION --> Cannot find transform for hunt point")
             rospy.signal_shutdown("tf Exception")
 
 
@@ -62,82 +64,84 @@ class GotoPoint():
             return SetBoolResponse(success = True,message = "Reverse mode ON")
         self.base_frame = 'base_footprint'
         return SetBoolResponse(success = True,message = "Reverse mode OFF")
-        
+
 
     def get_tf(self):
         try:
-            self.trans = self.tfBuffer.lookup_transform(self.base_frame, 'hunt_point', rospy.Time())
+            self.trans = self.tfBuffer.lookup_transform(self.base_frame, self.hunt_frame, rospy.Time())
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            rospy.logerr("NAVIGATE --> PROBLEMO WITH TFS ")
+            rospy.logerr("NAVIGATE --> PROBLEMO WITH TFS")
             self.r.sleep()
 
+    """
+    return the distance from turtle pose relative to hunting point
+    """
     def get_dist(self):
         self.dist = sqrt(self.trans.transform.translation.x ** 2 + self.trans.transform.translation.y ** 2)
 
-
+    """
+    return the direction error from turtle pose relative to hunting point
+    """
     def get_dir(self):
         self.dir = atan2(self.trans.transform.translation.y, self.trans.transform.translation.x)
 
 
-
+    """
+    return True if reached destination goal
+    """
     def reach_pos(self):
-        if self.dist > 0.05:
+        if self.dist > MIN_DIST_THRESHOLD:
             return False
         return True
-
+    """
+    if within direction margin return True
+    if error angle < threshhold return true so we also start moving forward 
+    """
     def reach_dir(self):
-        pass
+        return (abs(self.dir*TO_DEGREE)<MIN_DIR_THRESHOLD)
 
-    def set_cmd(self):
-        pass
+    """
+    calculate/return the correct cmd vel
+    """
+    def get_cmd(self):
+        move_cmd = Twist()
+        move_cmd.angular.z = self.dir
 
-    def ti_fasi(self):
-        # rospy.logdebug(f"NAVIGATION DIST--> {self.dist:.2f}")
-        # rospy.logdebug(f"NAVIGATION DIR --> {self.dir*TO_DEGREE:.2f}")
-        (position, rotation) = self.get_odom()
-        # rospy.logdebug(f"NAVIGATION ROT --> {rotation*TO_DEGREE}")
-        # rospy.logdebug(f"NAVIGATION TRA --> {self.trans.transform}")
+        # if within direction margin starting going forward
+        if(self.reach_dir()):
+            move_cmd.linear.x = 0.04
 
+        if(self.reach_pos()):
+            move_cmd.linear.x  = 0.0
+            move_cmd.angular.z = 0.0
+
+        if(self.reverse):
+            move_cmd.linear.x = - move_cmd.linear.x
+        
+        self.threshold_speed(move_cmd)
+        return move_cmd
+
+    def debug_msg(self):
         poutsa = [self.trans.transform.rotation.x,self.trans.transform.rotation.y,self.trans.transform.rotation.z,self.trans.transform.rotation.w]
         rospy.logdebug(f"------ NAVIGATION ------")
-        rospy.logdebug(f"NAVIGATION pou koitaw           --> {rotation*TO_DEGREE:.2f}")
-        rospy.logdebug(f"NAVIGATION pou prepei na koitaw --> {self.dir*TO_DEGREE:.2f}")
+        rospy.logdebug(f"NAVIGATION direction error      --> {self.dir*TO_DEGREE:.2f}")
         rospy.logdebug(f"NAVIGATION pou tou lew na koita --> {euler_from_quaternion(poutsa)[2]*TO_DEGREE:.2f}")
         rospy.logdebug(f"NAVIGATION target dist          --> {self.dist:.4f}")
         rospy.logdebug(f"NAVIGATION reverse MODE         --> {self.reverse}")
+        rospy.logdebug(f"------------------------")
 
 
     def update(self):
 
-        move_cmd = Twist()
-        (position, rotation) = self.get_odom()
-        self.linear_speed = 1
-        self.angular_speed = 1
 
         while not rospy.is_shutdown():
-
-
 
             self.get_tf()
             self.get_dist()
             self.get_dir()
-            # self.ti_fasi()
+            self.debug_msg()
 
-            move_cmd.angular.z = self.dir
-            if(abs(self.dir*TO_DEGREE)<15):
-                move_cmd.linear.x = 0.04
-
-            if(self.dist<0.05):
-                move_cmd.linear.x = 0.0
-
-            self.threshold_speed(move_cmd)
-
-
-            if(self.reverse):
-                move_cmd.linear.x = -move_cmd.linear.x
-
-
-            self.cmd_vel.publish(move_cmd)
+            self.cmd_vel.publish(self.get_cmd())
             self.r.sleep()
 
 
@@ -151,18 +155,11 @@ class GotoPoint():
         else:
             move_cmd.angular.z = max(move_cmd.angular.z, -MAX_ROTATION_SPEED)
 
-        move_cmd.linear.x = min(move_cmd.linear.x, MAX_ROTATION_SPEED)
+        if move_cmd.linear.x > 0:
+            move_cmd.linear.x = min(move_cmd.linear.x,  MAX_LINEAR_SPEED)
+        else:
+            move_cmd.linear.x = max(move_cmd.linear.x, -MAX_LINEAR_SPEED)
         
-
-
-    def get_odom(self):
-        try:
-            (trans, rot) = self.tf_listener.lookupTransform(self.odom_frame, self.base_frame, rospy.Time(0))
-            rotation = euler_from_quaternion(rot)
-        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
-            rospy.loginfo("NAVIGATION --> TF Exception")
-            return
-        return (Point(*trans), rotation[2])
 
     """
     callback function for shutdown of node
